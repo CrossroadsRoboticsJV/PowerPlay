133bfb5ca4387da9635c0c7ce6ab0638bd5ddb7c
benjaminshaheen <benashaheen@icloud.com>
2022-10-19 08:16:22 -0700

    Started Example OpMode

--- TeamCode/src/main/java/OpModes/ExampleOpMode.java
+++ TeamCode/src/main/java/OpModes/ExampleOpMode.java
@@ -1,2 +1,33 @@
-package OpModes;public class ExampleOpMode {
+package OpModes;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+
+@TeleOp
+public class ExampleOpMode extends LinearOpMode {
+
+    DcMotorEx frontLeft, frontRight, backLeft, backRight;
+
+    void initiate() {
+
+        frontLeft = hardwareMap.get(DcMotorEx.class, "frontLeft");
+        frontRight = hardwareMap.get(DcMotorEx.class, "frontRight");
+        backLeft = hardwareMap.get(DcMotorEx.class, "backLeft");
+        backRight = hardwareMap.get(DcMotorEx.class, "backRight");
+
+    }
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+
+        while(!isStopRequested()) {
+
+
+
+        }
+
+
+    }
+
 }
